16a7fdc995355492fa59548c996ca96a5d156a29
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-14 10:11:49 +0530

    Added: welcome message displayed

---
+++ Start.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program on Master Branch");
+    }
+}
